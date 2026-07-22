d8e58b79f1df0f1c53fea91acb53b6084bc983b3
user <70670632+stuxf@users.noreply.github.com>
2026-07-21 23:27:20 -0700

    🔧 chore: scripting da webbing

---
+++ scripts/build-web.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# make sure this always runs in the repo root so we can make the build folder properly
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
+
+mkdir -p build/web
+
+godot --headless --path . --export-release "Web" build/web/index.html
